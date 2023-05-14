d3dd8c699d1d259ea99f0a18f9bf87606f3fa3a3
Nancy <n.varyani4@gmail.com>
2023-05-14 16:55:50 +0530

    display method

--- app/src/main/java/com/example/rock_paper_scissors/MainActivity.java
+++ app/src/main/java/com/example/rock_paper_scissors/MainActivity.java
@@ -4,11 +4,33 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.rock_paper_scissors.databinding.ActivityMainBinding;
 
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
+    String n1,n2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding=ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+    }
+    public void display(){
+        Random rnd= new Random();
+        int n= rnd.nextInt(3);
+        if(n==0){
+            binding.robot.setImageResource(R.drawable.rock_hand);
+            binding.robot.setRotation(100);
+        }
+        else if(n==1){
+            binding.robot.setImageResource(R.drawable.paper_hand);
+            binding.robot.setRotation(100);
+        }
+        else if(n==2){
+            binding.robot.setImageResource(R.drawable.scissor_hand);
+            binding.robot.setRotation(100);
+        }
     }
 }
